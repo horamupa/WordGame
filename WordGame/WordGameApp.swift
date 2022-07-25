4520fa7130079b9edf1126b3e35250be6301c590
horamupa <horamupa@gmail.com>
2022-07-25 21:10:00 +0300

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by MM on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
